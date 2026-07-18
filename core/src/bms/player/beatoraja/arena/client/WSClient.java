0aa5b06602a4cbf750d1dccd1e20ba12132686f5
limb-soup <limbsoup@hotmail.com>
2026-07-18 20:40:02 +0000

    move to new enum CTS_FILE_TRANSFER

--- core/src/bms/player/beatoraja/arena/client/WSClient.java
+++ core/src/bms/player/beatoraja/arena/client/WSClient.java
@@ -162,6 +162,12 @@ public class WSClient extends WebSocketClient {
             case STC_CLIENT_REMOTE_ID -> Client.state.setRemoteId(new Address(value));
             case STC_MESSAGE -> {
                 Message message = new Message(value);
+		if (message.getMessage().equals("Hash check is disabled.")){
+		    Client.strictHash = false;
+		}
+		else if (message.getMessage().equals("Hash check is enabled.")){
+		    Client.strictHash = true;
+		}
                 if (message.isSystemMessage()) {
                     Lobby.addToLog(message.getMessage());
 		    return;
@@ -175,203 +181,201 @@ public class WSClient extends WebSocketClient {
             }
             /*case STC_MISSING_CHART -> {}*/
             case STC_ITEM -> {
-		if (data.length <4) return;
-		switch(data[3] >>> 4){
-		case 0x0:{
-		    // begin song download from server
-		    int fileSize = 0;
-		    fileSize += data[0] & 0xFF;
-		    fileSize += (int)(data[1] & 0xFF) << 8;
-		    fileSize += (int)(data[2] & 0xFF) << 16;
-		    fileSize += (int)(data[3] & 0x03) << 24;
-		    downloadBuf = new byte[fileSize];
-		    downloadBufIndex = 0;
-		    if (downloadBuf.length != downloadBufIndex){
-			byte[] dat = new byte[5];
-			dat[0] = (byte) ClientToServer.CTS_ITEM.getValue();
-			dat[1] = (byte) downloadBufIndex;
-			dat[2] = (byte) (downloadBufIndex >>> 8);
-			dat[3] = (byte) (downloadBufIndex >>> 16);
-			dat[4] = (byte) (downloadBufIndex >>> 24);
-			dat[4] |= 0x10;
-			super.send(dat);
-		    }
-
-		    return;
-		}
-		case 0x1:{
-		    // send song directory to server
-		    if (Client.state.getCurrentSongData() == null){
-			return;
-		    }
-		    Path dir = Path.of(Client.state.getCurrentSongData().getPath());
-		    dir = dir.getParent().normalize();
-		    File f_dir = new File(dir.toString());
-		    if (f_dir == null || !f_dir.isDirectory()) {
-			return;
-		    }
-		    String dirname = f_dir.getName() + "/";
-		    ByteArrayOutputStream byteOutput = new ByteArrayOutputStream();
-		    TarArchiveOutputStream tarOutput = new TarArchiveOutputStream(byteOutput);
-		    Stack<File> files = new Stack<>();
-		    Collections.addAll(files,f_dir.listFiles());
-		    File f = f_dir;
-		    TarArchiveEntry entry = new TarArchiveEntry(f, dirname);
-		    tarOutput.putArchiveEntry(entry);
-		    tarOutput.closeArchiveEntry();
-		    Path path_parent = f_dir.getParentFile().toPath();
-		    int file_size_sum = 0;
-		    while(!files.isEmpty() &&(f = files.pop())!= null){
-			dirname = path_parent.relativize(f.toPath()).toString();
-			if (f.isDirectory()){
-			    dirname = dirname + "/";
-			    entry = new TarArchiveEntry(f, dirname);
-			    tarOutput.putArchiveEntry(entry);
-			    tarOutput.closeArchiveEntry();
-			    File[] files_inner = f.listFiles();
-			    int file_count =  files_inner.length;
-			    for(int i = 0; i < file_count; ++i){
-				files.push(files_inner[i]);
-			    }
-			    continue;
-			}
-			entry = new TarArchiveEntry(f, dirname);
-			entry.setSize(f.length());
-			file_size_sum += f.length();
-			tarOutput.putArchiveEntry(entry);
-			tarOutput.write(IOUtils.toByteArray(new FileInputStream(f)));
-			tarOutput.closeArchiveEntry();
-		    }
-		    tarOutput.close();
-		    int size = byteOutput.size();
-		    if (size >= (1 << 27)){
-			Lobby.addToLog(String.format("file size limit exceeded"));
-		    }
-		    byte[] dat = new byte[5+size];
-		    dat[0] = (byte) ClientToServer.CTS_ITEM.getValue();
-		    dat[1] = (byte) size;
-		    dat[2] = (byte) (size >>> 8);
-		    dat[3] = (byte) (size >>> 16);
-		    dat[4] = (byte) (size >>> 24);
-		    if (this.isOpen() && Client.connected.get()) {
-			Lobby.addToLog("Sending bms to server...");
-			System.arraycopy(byteOutput.toByteArray(), 0, dat, 5, size);
-			super.send(dat);
-		    }
-		    return;
-		}
-		case 0x2:{	// receive partial segment from server
-		    int fileSize = 0;
-		    fileSize += data[0] & 0xFF;
-		    fileSize += (int)(data[1] & 0xFF) << 8;
-		    fileSize += (int)(data[2] & 0xFF) << 16;
-		    fileSize += (int)(data[3] & 0x03) << 24;
-		    if (data.length != fileSize + 4 || downloadBufIndex + fileSize >= downloadBuf.length){
-			System.out.println("Error in segment length");
-			System.out.println("index " + downloadBufIndex + ", segment  " + fileSize + ", buffer length " +
-					   downloadBuf.length + ", message length " + data.length + 4);
-			return;
-		    }
-		    System.arraycopy(data, 4, downloadBuf, downloadBufIndex, fileSize);
-		    downloadBufIndex += fileSize;
+                // TODO: Item
+            }
+            case STC_ITEM_SETTINGS -> {
+                // TODO: Item
+            }
+	case STC_FILE_TRANSFER -> {
+	    if (data.length <4) return;
+	    switch(data[3] >>> 4){
+	    case 0x0:{
+		// begin song download from server
+		int fileSize = 0;
+		fileSize += data[0] & 0xFF;
+		fileSize += (int)(data[1] & 0xFF) << 8;
+		fileSize += (int)(data[2] & 0xFF) << 16;
+		fileSize += (int)(data[3] & 0x03) << 24;
+		downloadBuf = new byte[fileSize];
+		downloadBufIndex = 0;
+		if (downloadBuf.length != downloadBufIndex){
 		    byte[] dat = new byte[5];
-		    dat[0] = (byte) ClientToServer.CTS_ITEM.getValue();
+		    dat[0] = (byte) ClientToServer.CTS_FILE_TRANSFER.getValue();
 		    dat[1] = (byte) downloadBufIndex;
 		    dat[2] = (byte) (downloadBufIndex >>> 8);
 		    dat[3] = (byte) (downloadBufIndex >>> 16);
 		    dat[4] = (byte) (downloadBufIndex >>> 24);
 		    dat[4] |= 0x10;
 		    super.send(dat);
+		}
+
+		return;
+	    }
+	    case 0x1:{
+		// send song directory to server
+		if (Client.state.getCurrentSongData() == null){
 		    return;
 		}
-		case 0x3:{	// receive final segment from server
-		    int fileSize = 0;
-		    fileSize += data[0] & 0xFF;
-		    fileSize += (int)(data[1] & 0xFF) << 8;
-		    fileSize += (int)(data[2] & 0xFF) << 16;
-		    fileSize += (int)(data[3] & 0x03) << 24;
-		    if (data.length != fileSize + 4 || downloadBufIndex + fileSize != downloadBuf.length){
-			System.out.println("index " + downloadBufIndex + ", segment  " + fileSize + ", buffer length " +
-					   downloadBuf.length + ", message length " + data.length + 4);
-			return;
+		Path dir = Path.of(Client.state.getCurrentSongData().getPath());
+		dir = dir.getParent().normalize();
+		File f_dir = new File(dir.toString());
+		if (f_dir == null || !f_dir.isDirectory()) {
+		    return;
+		}
+		String dirname = f_dir.getName() + "/";
+		ByteArrayOutputStream byteOutput = new ByteArrayOutputStream();
+		TarArchiveOutputStream tarOutput = new TarArchiveOutputStream(byteOutput);
+		Stack<File> files = new Stack<>();
+		Collections.addAll(files,f_dir.listFiles());
+		File f = f_dir;
+		TarArchiveEntry entry = new TarArchiveEntry(f, dirname);
+		tarOutput.putArchiveEntry(entry);
+		tarOutput.closeArchiveEntry();
+		Path path_parent = f_dir.getParentFile().toPath();
+		int file_size_sum = 0;
+		while(!files.isEmpty() &&(f = files.pop())!= null){
+		    dirname = path_parent.relativize(f.toPath()).toString();
+		    if (f.isDirectory()){
+			dirname = dirname + "/";
+			entry = new TarArchiveEntry(f, dirname);
+			tarOutput.putArchiveEntry(entry);
+			tarOutput.closeArchiveEntry();
+			File[] files_inner = f.listFiles();
+			int file_count =  files_inner.length;
+			for(int i = 0; i < file_count; ++i){
+			    files.push(files_inner[i]);
+			}
+			continue;
 		    }
-		    System.arraycopy(data, 4, downloadBuf, downloadBufIndex, fileSize);
-		    downloadBufIndex += fileSize;
-		    TarArchiveInputStream tarIn = new TarArchiveInputStream(new ByteArrayInputStream(downloadBuf));
-		    File f = new File("arena_download");
-		    f.mkdir();
-		    TarArchiveEntry entry = null;
-		    int file_size_sum = 0;
-		    bmsDirectory = null;
-		    while((entry  = tarIn.getNextEntry()) != null){
-			String name = "arena_download/" + entry.getName();
-			f = new File(name);
-			if (!f.toPath().startsWith("arena_download")) {
-			    System.out.println("Rejecting file: " +name);
-			    continue;
-			}
-			if (entry.isDirectory()){
-			    f.mkdir();
-			    if (bmsDirectory == null)
-				bmsDirectory = name;
-			    continue;
-			}
-			if (!entry.isFile()){
-			    System.out.println("Unexpected entry in tar is not a file: " + entry.getName());
-			    continue;
-			}
-			f = new File(name);
-			FileOutputStream fileOut = new FileOutputStream(f);
-			byte[] fbuf = new byte[(int) entry.getSize()];
-			if (tarIn.read(fbuf, 0, (int) entry.getSize()) != entry.getSize())
-			    System.out.println("Failed to read entry: " + entry.getName());
-			fileOut.write(fbuf);
-			fileOut.close();
-			file_size_sum += entry.getSize();
-		    }
-		    MainController.pushOneShotAfterRenderTask(main -> {
-			    main.updateSong(bmsDirectory,true);
-			    String md5 = Client.state.getSelectedSongRemote().getMd5();
-			    SongData[] songDatas = main.getSongDatabase().getSongDatas(new String[]{md5});
-			    for (int i = 0; i < 5; ++i){
-				try{
-				    Thread.sleep(100);
-				}
-				catch (Exception e){
-				}
-				songDatas = main.getSongDatabase().getSongDatas(new String[]{md5});
-				if (songDatas != null && songDatas.length > 0) {
-				    Client.state.setLobbySongData(songDatas[0]);
-				    break;
-				}
-			    }
-			});
-		    Lobby.addToLog(String.format("bms download complete"));
-		    return;
+		    entry = new TarArchiveEntry(f, dirname);
+		    entry.setSize(f.length());
+		    file_size_sum += f.length();
+		    tarOutput.putArchiveEntry(entry);
+		    tarOutput.write(IOUtils.toByteArray(new FileInputStream(f)));
+		    tarOutput.closeArchiveEntry();
 		}
-		case 0x4:{
-		    Client.strictHash = (data[0] != 0);
-		    Lobby.addToLog(String.format("[#] bms hash is %s.", Client.strictHash ? "enforced" : "not enforced"));
-		    return;
+		tarOutput.close();
+		int size = byteOutput.size();
+		if (size >= (1 << 27)){
+		    Lobby.addToLog(String.format("file size limit exceeded"));
 		}
-		case 0x5:
-		case 0x6:
-		case 0x7:
-		case 0x8:
-		case 0x9:
-		case 0xA:
-		case 0xB:
-		case 0xC:
-		case 0xD:
-		case 0xE:
-		case 0xF:
+		byte[] dat = new byte[5+size];
+		dat[0] = (byte) ClientToServer.CTS_FILE_TRANSFER.getValue();
+		dat[1] = (byte) size;
+		dat[2] = (byte) (size >>> 8);
+		dat[3] = (byte) (size >>> 16);
+		dat[4] = (byte) (size >>> 24);
+		if (this.isOpen() && Client.connected.get()) {
+		    Lobby.addToLog("Sending bms to server...");
+		    System.arraycopy(byteOutput.toByteArray(), 0, dat, 5, size);
+		    super.send(dat);
 		}
 		return;
-                // TODO: Item
-            }
-            case STC_ITEM_SETTINGS -> {
-                // TODO: Item
-            }
+	    }
+	    case 0x2:{	// receive partial segment from server
+		int fileSize = 0;
+		fileSize += data[0] & 0xFF;
+		fileSize += (int)(data[1] & 0xFF) << 8;
+		fileSize += (int)(data[2] & 0xFF) << 16;
+		fileSize += (int)(data[3] & 0x03) << 24;
+		if (data.length != fileSize + 4 || downloadBufIndex + fileSize >= downloadBuf.length){
+		    System.out.println("Error in segment length");
+		    System.out.println("index " + downloadBufIndex + ", segment  " + fileSize + ", buffer length " +
+				       downloadBuf.length + ", message length " + data.length + 4);
+		    return;
+		}
+		System.arraycopy(data, 4, downloadBuf, downloadBufIndex, fileSize);
+		downloadBufIndex += fileSize;
+		byte[] dat = new byte[5];
+		dat[0] = (byte) ClientToServer.CTS_FILE_TRANSFER.getValue();
+		dat[1] = (byte) downloadBufIndex;
+		dat[2] = (byte) (downloadBufIndex >>> 8);
+		dat[3] = (byte) (downloadBufIndex >>> 16);
+		dat[4] = (byte) (downloadBufIndex >>> 24);
+		dat[4] |= 0x10;
+		super.send(dat);
+		return;
+	    }
+	    case 0x3:{	// receive final segment from server
+		int fileSize = 0;
+		fileSize += data[0] & 0xFF;
+		fileSize += (int)(data[1] & 0xFF) << 8;
+		fileSize += (int)(data[2] & 0xFF) << 16;
+		fileSize += (int)(data[3] & 0x03) << 24;
+		if (data.length != fileSize + 4 || downloadBufIndex + fileSize != downloadBuf.length){
+		    System.out.println("index " + downloadBufIndex + ", segment  " + fileSize + ", buffer length " +
+				       downloadBuf.length + ", message length " + data.length + 4);
+		    return;
+		}
+		System.arraycopy(data, 4, downloadBuf, downloadBufIndex, fileSize);
+		downloadBufIndex += fileSize;
+		TarArchiveInputStream tarIn = new TarArchiveInputStream(new ByteArrayInputStream(downloadBuf));
+		File f = new File("arena_download");
+		f.mkdir();
+		TarArchiveEntry entry = null;
+		int file_size_sum = 0;
+		bmsDirectory = null;
+		while((entry  = tarIn.getNextEntry()) != null){
+		    String name = "arena_download/" + entry.getName();
+		    f = new File(name);
+		    if (!f.toPath().startsWith("arena_download")) {
+			System.out.println("Rejecting file: " +name);
+			continue;
+		    }
+		    if (entry.isDirectory()){
+			f.mkdir();
+			if (bmsDirectory == null)
+			    bmsDirectory = name;
+			continue;
+		    }
+		    if (!entry.isFile()){
+			System.out.println("Unexpected entry in tar is not a file: " + entry.getName());
+			continue;
+		    }
+		    f = new File(name);
+		    FileOutputStream fileOut = new FileOutputStream(f);
+		    byte[] fbuf = new byte[(int) entry.getSize()];
+		    if (tarIn.read(fbuf, 0, (int) entry.getSize()) != entry.getSize())
+			System.out.println("Failed to read entry: " + entry.getName());
+		    fileOut.write(fbuf);
+		    fileOut.close();
+		    file_size_sum += entry.getSize();
+		}
+		MainController.pushOneShotAfterRenderTask(main -> {
+			main.updateSong(bmsDirectory,true);
+			String md5 = Client.state.getSelectedSongRemote().getMd5();
+			SongData[] songDatas = main.getSongDatabase().getSongDatas(new String[]{md5});
+			for (int i = 0; i < 5; ++i){
+			    try{
+				Thread.sleep(100);
+			    }
+			    catch (Exception e){
+			    }
+			    songDatas = main.getSongDatabase().getSongDatas(new String[]{md5});
+			    if (songDatas != null && songDatas.length > 0) {
+				Client.state.setLobbySongData(songDatas[0]);
+				break;
+			    }
+			}
+		    });
+		Lobby.addToLog(String.format("bms download complete"));
+		return;
+	    }
+	    case 0x4:
+	    case 0x5:
+	    case 0x6:
+	    case 0x7:
+	    case 0x8:
+	    case 0x9:
+	    case 0xA:
+	    case 0xB:
+	    case 0xC:
+	    case 0xD:
+	    case 0xE:
+	    case 0xF:
+	    }
+	    return;
+	}
             default -> ImGuiNotify.warning("unexpected S->C message id: " + id);
         }
     }
